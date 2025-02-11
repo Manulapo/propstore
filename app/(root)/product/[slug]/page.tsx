bb30efd48b1bdfb929a54d664b819bd399c6cdb0
Manulapo <manulaporta98@gmail.com>
2025-02-11 14:04:30 +0100

    ..

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -32,7 +32,7 @@ const ProductDetailsPage = async (props: {
                             <h1 className="h3-bold">
                                 {product.name}
                             </h1>
-                            <p>{product.rating}of {product.numReviews} Reviews</p>
+                            <p>{product.rating} of {product.numReviews} Reviews</p>
                             <div className="flex flex-col sm:flex-row sm:items-center gap-3">
                                 <ProductPrice price={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2" />
                             </div>
