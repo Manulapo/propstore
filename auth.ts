058dbe9a53494aa0fcda8103de84d61d614a1359
Manulapo <manulaporta98@gmail.com>
2025-02-13 17:23:30 +0100

    test

--- auth.ts
+++ auth.ts
@@ -1,111 +1,165 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import NextAuth, { NextAuthConfig } from 'next-auth';
-import { prisma } from '@/db/prisma'
+import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import CredentialsProvider from "next-auth/providers/credentials"
-import { compareSync } from 'bcrypt-ts-edge';
+import { prisma } from '@/db/prisma';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from './lib/encrypt';
+import type { NextAuthConfig } from 'next-auth';
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export const config = {
-    pages: {
-        signIn: '/sign-in', 
-        error: '/sign-in', 
-    },
-    session: {
-        strategy: 'jwt', 
-        maxAge: 30 * 24 * 60 * 60, 
-    },
-    adapter: PrismaAdapter(prisma), 
-    providers: [
-        CredentialsProvider({
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) return null;
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
 
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.email as string },
-                    select: { id: true, name: true, email: true, password: true, role: true }
-                });
-
-                if (!user || !user.password) return null;
-
-                const isValidPassword = compareSync(credentials.password as string, user.password);
-                if (!isValidPassword) return null;
-
-                return { id: user.id, name: user.name, email: user.email, role: user.role };
-            }
-        })
-    ],
-    callbacks: {
-        async session(params: any) {
-            return handleSession(params);
-        },
-        async jwt(params: any) {
-            return handleJwt(params);
-        },
-        async authorized(params: any) {
-            return handleAuthorized(params);
-        }
-    },
-} satisfies NextAuthConfig;
-
-async function handleSession({ session, user, trigger, token }: any) {
-    // Set user ID from the token
-    session.user.id = token.sub; // the sub is the user id that is stored in the token
-    session.user.role = token.role;
-    session.user.name = token.name;
-
-    // If there is an update, set the user name (our app allows to change name)
-    if (trigger === 'update') {
-        session.user.name = user.name;
-    }
-
-    return session;
-}
-
-async function handleJwt({ token, user }: any) {
-    token.role = user.role;
-
-    // User has no name, use the first part of the email as the name
-    if (!user.name || user.name === 'NO_NAME') {
-        const name = user.email.split('@')[0];
-        token.name = name;
-        // Update database with the new name
-        await prisma.user.update({
-            where: { id: user.id },
-            data: { name: token.name }
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
         });
 
-        return token;
-    }
-}
+        // Check if user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = await compare(
+            credentials.password as string,
+            user.password
+          );
 
-async function handleAuthorized({ request }: any) {
-    // Check for session cart cookie
-    if (!request.cookies.get('sessionCartId')) {
-        // Create a session cart ID
+          // If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // If user does not exist or password does not match return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      // If there is an update, set the user name
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
+      }
+
+      // Handle session updates
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
+
+      return token;
+    },
+    authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
+      // Check for session cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate new session cart id cookie
         const sessionCartId = crypto.randomUUID();
 
-        // Clone request headers
+        // Clone the req headers
         const newRequestHeaders = new Headers(request.headers);
 
-        // Create new response and add new headers
+        // Create new response and add the new headers
         const response = NextResponse.next({
-            request: {
-                headers: newRequestHeaders
-            }
+          request: {
+            headers: newRequestHeaders,
+          },
         });
 
-        // Set cookie
+        // Set newly generated sessionCartId in the response cookies
         response.cookies.set('sessionCartId', sessionCartId);
 
         return response;
-    } else {
+      } else {
         return true;
-    }
-}
+      }
+    },
+  },
+} satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
