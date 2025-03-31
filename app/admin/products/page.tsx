f2c75ef7288e57a321a49d231196bf9dbfad93e8
Manulapo <manulaporta98@gmail.com>
2025-03-31 11:51:27 +0200

    feat: implement product listing with pagination and search functionality for admin users

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,29 @@
 import { requireAdmin } from "@/lib/auth-guard";
+import { getAllProducts } from "@/lib/actions/product.actions";
+import { formatCurrency, formatId } from "@/lib/utils";
 
-const AdminProductPage = async () => {
-      await requireAdmin(); // Ensure the user is an admin
-    
-    return ( <></> );
-}
- 
+const AdminProductPage = async (props: {
+  searchParams: Promise<{ page: string; query: string; category: string }>;
+}) => {
+  await requireAdmin(); // Ensure the user is an admin
+  const searchParams = await props.searchParams; // Await the searchParams promise if needed
+  const page = Number(searchParams.page) || 1; // Default to page 1 if not provided
+  const searchText = searchParams.query || ""; // Default to empty string if not provided
+  const category = searchParams.category || ""; // Default to empty string if not provided
+
+  const product = await getAllProducts({
+    query: searchText,
+    page,
+    category,
+  });
+
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Products</h1>
+      </div>
+    </div>
+  );
+};
+
 export default AdminProductPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,32 +1,58 @@
-'use server';
+"use server";
 
-import { convertPrismaObj } from '../utils';
-import { LATEST_PRODCUCT_LIMIT } from '../constants';
-import { prisma } from '@/db/prisma';
+import { convertPrismaObj } from "../utils";
+import { LATEST_PRODCUCT_LIMIT, PAGE_SIZE } from "../constants";
+import { prisma } from "@/db/prisma";
 
 export async function getLatestProducts(limit: number = LATEST_PRODCUCT_LIMIT) {
-    try {
-        const products = await prisma.product.findMany({
-            take: limit,
-            orderBy: { createdAt: 'desc' },
-        });
+  try {
+    const products = await prisma.product.findMany({
+      take: limit,
+      orderBy: { createdAt: "desc" },
+    });
 
-        return convertPrismaObj(products);
-    } catch (error) {
-        console.error('Failed to fetch latest products:', error);
-        throw new Error('Failed to fetch latest products');
-    }
+    return convertPrismaObj(products);
+  } catch (error) {
+    console.error("Failed to fetch latest products:", error);
+    throw new Error("Failed to fetch latest products");
+  }
 }
 
 export async function getProductBySlug(slug: string) {
-    try {
-        const product = await prisma.product.findFirst({
-            where: { slug },
-        });
+  try {
+    const product = await prisma.product.findFirst({
+      where: { slug },
+    });
 
-        return convertPrismaObj(product);
-    } catch (error) {
-        console.error('Failed to fetch product by slug:', error);
-        throw new Error('Failed to fetch product by slug');
-    }
+    return convertPrismaObj(product);
+  } catch (error) {
+    console.error("Failed to fetch product by slug:", error);
+    throw new Error("Failed to fetch product by slug");
+  }
+}
+
+// return all the products with pagination
+export async function getAllProducts({
+  query,
+  page,
+  limit = PAGE_SIZE,
+  category,
+}: {
+  query: string;
+  page: number;
+  limit?: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (Number(page) - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data: convertPrismaObj(data),
+    totalPage: Math.ceil(dataCount / limit),
+    currentPage: page,
+  };
 }
