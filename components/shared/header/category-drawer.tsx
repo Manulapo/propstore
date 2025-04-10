e49445983220119ec55ff617f6145fd95662e2e4
Manulapo <manulaporta98@gmail.com>
2025-04-10 12:32:42 +0200

    feat: add CategoryDrawer component for category selection and integrate with header feat: implement Drawer component using vaul for improved UI/UX feat: add getAllCategories function to fetch categories from the database

---
+++ components/shared/header/category-drawer.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { getAllCategories } from "@/lib/actions/product.actions";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+
+const CategoryDrawer = async () => {
+  const categories = await getAllCategories();
+
+  return (
+    <div>
+        <Drawer direction="left">
+          <DrawerTrigger asChild>
+            <Button variant="outline">
+              <MenuIcon />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent className="h-full max-w-sm">
+            <DrawerHeader>
+              <div className="mb-4">
+                  <DrawerTitle>Select a Category</DrawerTitle>
+              </div>
+              <div className="space-y-1">
+                {categories.map(({ category, _count }) => (
+                  <Button
+                    key={category}
+                    variant="ghost"
+                    className="w-full flex-start"
+                    asChild
+                  >
+                    <DrawerClose asChild>
+                      <Link href={`/search?category=${category}`}>
+                        {category} ({_count.toString()})
+                      </Link>
+                    </DrawerClose>
+                  </Button>
+                ))}
+              </div>
+            </DrawerHeader>
+          </DrawerContent>
+        </Drawer>
+    </div>
+  );
+};
+
+export default CategoryDrawer;
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,16 +1,16 @@
-'use client';
-
 import { APP_NAME } from "@/lib/constants";
 import Image from 'next/image';
 import Link from "next/link";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
 
 export default function Header() {
     return (
         <header className="w-full border-b">
             <div className="container mx-auto px-4 py-4 flex items-center justify-between">
                 <div className="flex items-center gap-2">
-                    <Link href='/' className="flex items-center">
+                    <CategoryDrawer />
+                    <Link href='/' className="flex items-center ml-4">
                         <Image
                             src='/images/logo.svg'
                             height={48}
