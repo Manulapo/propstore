6d4a04936b59c68f92dcbfc0ec2ac7217477f8f2
Manulapo <manulaporta98@gmail.com>
2025-04-10 18:28:44 +0200

    refactor: clean up ProductCarousel component by commenting out unused Autoplay plugin

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -5,14 +5,22 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { ProductType } from "@/types";
-import Autoplay from "embla-carousel-autoplay";
+// import Autoplay from "embla-carousel-autoplay";
 
 const ProductCarousel = ({ data }: { data: ProductType[] }) => {
-  return <Carousel className="w-full mb-12" opts={{loop:true}} plugins={[Autoplay({
-    delay: 2000,
-    stopOnInteraction: true,
-    stopOnMouseEnter: true,
-  })]}></Carousel>;
+  return (
+    <Carousel
+      className="w-full mb-12"
+      opts={{ loop: true }}
+      plugins={[
+        // Autoplay({
+        //   delay: 2000,
+        //   stopOnInteraction: true,
+        //   stopOnMouseEnter: true,
+        // }),
+      ]}
+    ></Carousel>
+  );
 };
 
 export default ProductCarousel;
