afdac244792220927920e676f71c64d4c9949f88
Manulapo <manulaporta98@gmail.com>
2025-04-11 12:56:41 +0200

    feat: integrate Rating component for product ratings display in ProductDetailsPage and ProductCard

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -3,41 +3,42 @@ import Link from "next/link";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import ProductPrice from "./product-price";
 import { ProductType } from "@/types/index";
+import Rating from "./ratings";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
-    return (
-        <Card className="overflow-hidden mx-3">
-            <CardHeader className='p-0 items-center'>
-                <Link href={`/product/${product.slug}`}>
-                    <Image
-                        priority={true}
-                        src={product.images![0]}
-                        alt={product.name}
-                        className='aspect-square object-cover rounded'
-                        height={300}
-                        width={300}
-                    />
-                </Link>
-            </CardHeader>
-            <CardContent className="p-4 grid gap-4">
-                <span className="text-sm text-muted-foreground">{product.brand}</span>
-                <Link
-                    href={`/product/${product.slug}`}
-                    className="text-sm underline-offset-4 hover:underline"
-                >
-                    <h3 className="font-semibold truncate ">{product.name}</h3>
-                </Link>
-                <div className="flex justify-between items-center mt-4">
-                    <p>{Number(product.rating)} stars</p>
-                    {product.stock > 0 ? (
-                        <ProductPrice price={Number(product.price)} />
-                    ) : (
-                        <p className='text-destructive'>Out of Stock</p>
-                    )}
-                </div>
-            </CardContent>
-        </Card>
-    );
-}
+  return (
+    <Card className="overflow-hidden mx-3">
+      <CardHeader className="p-0 items-center">
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            priority={true}
+            src={product.images![0]}
+            alt={product.name}
+            className="aspect-square object-cover rounded"
+            height={300}
+            width={300}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <span className="text-sm text-muted-foreground">{product.brand}</span>
+        <Link
+          href={`/product/${product.slug}`}
+          className="text-sm underline-offset-4 hover:underline"
+        >
+          <h3 className="font-semibold truncate ">{product.name}</h3>
+        </Link>
+        <div className="flex justify-between items-center mt-4">
+          <Rating value={Number(product.rating)} />
+          {product.stock > 0 ? (
+            <ProductPrice price={Number(product.price)} />
+          ) : (
+            <p className="text-destructive">Out of Stock</p>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default ProductCard;
