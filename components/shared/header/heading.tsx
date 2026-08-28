346f8c143c1042bc11f97553f4d43e980bcda1f6
Manulapo <manulaporta98@gmail.com>
2026-08-28 14:58:01 +0200

    fix: render heading icons directly

--- components/shared/header/heading.tsx
+++ components/shared/header/heading.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-
 const Heading = ({
   icon,
   title,
 }: {
-  icon?: React.ReactElement;
+  icon?: React.ReactNode;
   title?: string;
 }) => {
   return (
     <div className="flex gap-2 items-center mb-4">
-      {icon && React.cloneElement(icon)}
+      {icon}
       {title && <h2 className="text-2xl font-bold">{title}</h2>}
     </div>
   );
